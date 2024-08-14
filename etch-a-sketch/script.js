56b26e309955135f2a077f6a8ae910f4b2eadd49
Nel <nel.renaudin@gmail.com>
2024-08-14 10:07:15 +0200

    eas: implement creating new grid with specified size

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,10 +1,15 @@
 const grid = document.querySelector("#grid");
+const btn = document.querySelector("button");
 
 function createGrid(gridElem, size) {
+  const gridWidth = gridElem.offsetWidth;
+
   gridElem.innerHTML = null;
 
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement("div");
+    square.style.width = gridWidth / size + "px";
+    square.style.height = gridWidth / size + "px";
     gridElem.appendChild(square);
   }
 }
@@ -19,6 +24,17 @@ function handleGridHover(event) {
   target.style.backgroundColor = "black";
 }
 
-createGrid(grid, 16);
+function handleBtnClick() {
+  let size;
+
+  while (!size || size < 2 || size > 100) {
+    size = +prompt("Enter grid size (between 2 and 100)");
+  }
+
+  createGrid(grid, size);
+}
 
 grid.addEventListener("mouseover", handleGridHover);
+btn.addEventListener("click", handleBtnClick);
+
+createGrid(grid, 16);
