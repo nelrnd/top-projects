0a6ec0568c7a69b1183ff88256ec1e5ae1d168dd
Nel <nel.renaudin@gmail.com>
2024-09-26 10:58:01 +0200

    configure two webpack config for development and production

--- restaurant-page/webpack.common.js
+++ restaurant-page/webpack.common.js
@@ -2,16 +2,12 @@ const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
