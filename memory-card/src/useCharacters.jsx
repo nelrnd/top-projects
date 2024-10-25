04c48540dd589645dd6f5b839dec37658d6ea3f2
Nel <nel.renaudin@gmail.com>
2024-10-25 14:19:26 +0200

    memory-card: Create useCharacters custom hook

---
+++ memory-card/src/useCharacters.jsx
@@ -0,0 +1,45 @@
+export default function useCharacters() {
+  const [characters, setCharacters] = useState([])
+
+  const excludeList = [
+    141, 166, 195, 325, 396, 400, 1359, 559, 593, 627, 735, 736,
+  ]
+
+  function isNotInExcludeList(character) {
+    return !excludeList.includes(character.id)
+  }
+
+  function modelCharacter(character) {
+    return {
+      id: character.id,
+      name: character.name,
+      image: character.images[0],
+    }
+  }
+
+  function shuffle() {
+    const charactersCopy = [...characters]
+    let currentIndex = characters.length
+    while (currentIndex != 0) {
+      let randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex--
+      ;[charactersCopy[currentIndex], charactersCopy[randomIndex]] = [
+        charactersCopy[randomIndex],
+        charactersCopy[currentIndex],
+      ]
+    }
+    setCharacters(charactersCopy)
+  }
+
+  useEffect(() => {
+    fetch("https://narutodb.xyz/api/akatsuki")
+      .then((res) => res.json())
+      .then((data) => data.akatsuki)
+      .then((characters) => characters.filter(isNotInExcludeList))
+      .then((characters) => characters.map(modelCharacter))
+      .then((characters) => setCharacters(characters))
+      .catch((err) => console.log(err))
+  }, [])
+
+  return [characters, shuffle]
+}
