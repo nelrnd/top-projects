acd4f778ec10b0d12dc347e233f4c867e1f816b5
Nel <nel.renaudin@gmail.com>
2024-11-19 09:48:30 +0100

    remove unecessary express.static, and create getPage fn

--- basic-informational-site/app.js
+++ basic-informational-site/app.js
@@ -1,18 +1,19 @@
 require("dotenv").config()
 const express = require("express")
+const path = require("path")
 const PORT = process.env.PORT || 3000
 const app = express()
 
-app.use(express.static("pages"))
+app.get("/", (req, res) => res.sendFile(getPage("index")))
 
-app.get("/", (req, res) => res.sendFile("index.html", { root: "pages" }))
+app.get("/about", (req, res) => res.sendFile(getPage("about")))
 
-app.get("/about", (req, res) => res.sendFile("about.html", { root: "pages" }))
+app.get("/contact-me", (req, res) => res.sendFile(getPage("contact-me")))
 
-app.get("/contact-me", (req, res) =>
-  res.sendFile("contact-me.html", { root: "pages" })
-)
+app.use((req, res) => res.status(404).sendFile(getPage("404")))
 
-app.use((req, res) => res.status(404).sendFile("404.html", { root: "pages" }))
+function getPage(name) {
+  return path.join(__dirname, "pages", `${name}.html`)
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
