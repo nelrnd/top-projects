cd49af447470a885a8fb0b106b44e89822493af5
Nel <nel.renaudin@gmail.com>
2024-11-19 20:01:31 +0100

    create POST:/new route handler

--- mini-message-board/app.js
+++ mini-message-board/app.js
@@ -6,6 +6,8 @@ const app = express()
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
+app.use(express.urlencoded({ extended: true }))
+
 const messages = [
   {
     text: "Hi there!",
@@ -22,5 +24,17 @@ const messages = [
 app.get("/", (req, res) => res.render("index", { messages }))
 
 app.get("/new", (req, res) => res.render("form"))
+
+app.post("/new", (req, res) => {
+  const message = {
+    user: req.body.author,
+    text: req.body.text,
+    added: new Date(),
+  }
+
+  messages.push(message)
+
+  res.redirect("/")
+})
 
 app.listen(2001)
