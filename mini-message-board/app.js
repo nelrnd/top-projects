aac6f6396eb776c3c41d24b2326ba01eda7e4351
Nel <nel.renaudin@gmail.com>
2024-11-19 19:48:46 +0100

    display messages in index

--- mini-message-board/app.js
+++ mini-message-board/app.js
@@ -1,7 +1,24 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
 
-app.get("/", (req, res) => res.send("Message Board"))
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+]
+
+app.get("/", (req, res) => res.render("index", { messages }))
 
 app.listen(2001)
