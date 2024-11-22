afbb375db17da4d0e804145ec859c11644e20932
Nel <nel.renaudin@gmail.com>
2024-11-22 15:21:26 +0100

    Install and configure tailwindcss

--- inventory-application/app.js
+++ inventory-application/app.js
@@ -9,6 +9,7 @@ app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
 app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, "public")))
 
 const indexRouter = require("./routes/indexRouter")
 const itemRouter = require("./routes/itemRouter")
