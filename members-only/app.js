e167dd48cc0e13c2a64f5e380249116cb1207de3
Nel <nel.renaudin@gmail.com>
2024-11-23 17:43:48 +0100

    Configure database, sessions and passportjs

--- members-only/app.js
+++ members-only/app.js
@@ -2,6 +2,9 @@ require("dotenv").config()
 const path = require("path")
 const express = require("express")
 const session = require("express-session")
+const pgSession = require("connect-pg-simple")(session)
+const passport = require("passport")
+const pool = require("./database/pool")
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -13,11 +16,21 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(
   session({
+    store: new pgSession({ pool, tableName: "sessions" }),
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // for 30 days
   })
 )
+
+require("./config/passport")
+app.use(passport.session())
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user
+  next()
+})
 
 app.get("/", (req, res) => res.send("Hello?"))
 
---
+++ members-only/config/passport.js
@@ -0,0 +1,43 @@
+const passport = require("passport")
+const LocalStrategy = require("passport-local").Strategy
+const bcrypt = require("bcryptjs")
+const pool = require("../database/pool")
+
+const customFields = {
+  usernameField: "email",
+}
+
+const verifyCallback = async (email, password, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ])
+    const user = rows[0]
+    if (!user) {
+      return done(null, false, { message: "Incorrect username" })
+    }
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) {
+      return done(null, false, { message: "Incorrect password" })
+    }
+    return done(null, user)
+  } catch (err) {
+    return done(err)
+  }
+}
+
+passport.use(new LocalStrategy(customFields, verifyCallback))
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id])
+    const user = rows[0]
+    done(null, user)
+  } catch (err) {
+    done(err)
+  }
+})
