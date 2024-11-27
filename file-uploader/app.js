f4b7e35b6bc02ba5bc78663bb97aeb335e942202
Nel <nel.renaudin@gmail.com>
2024-11-27 22:17:27 +0100

    Implement session store

--- file-uploader/app.js
+++ file-uploader/app.js
@@ -3,6 +3,8 @@ const path = require("path")
 const express = require("express")
 const session = require("express-session")
 const passport = require("passport")
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store")
+const { PrismaClient } = require("@prisma/client")
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -19,6 +21,11 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 }, // for 30 days
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000, // 2 minutes
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
   })
 )
 
